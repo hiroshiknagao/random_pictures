838c0c64609e1d7431cf1afb86b3f8e95e041c28
hiroshi nagao <hi_1205@hotmail.com>
2024-07-15 15:46:46 +0900

    add functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+const imageContainer = document.querySelector(".image-container");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    imageNum = 10;
+    addNewImages();
+});
+
+function addNewImages(){
+    for (let index = 0; index < imageNum; index++) {
+      const newImg = document.createElement("img")
+      newImg.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*1000)}`;
+      imageContainer.appendChild(newImg);
+    }
+};
